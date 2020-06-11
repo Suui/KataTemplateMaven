926d2d1f93e8c2923eb27d30c5734b7e24c5bfdf
Daniel Ojeda <daniel@codesai.com>
2020-06-11 12:17:18 +0200

    starting point

---
+++ src/test/java/SomethingTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomethingTest {
+
+    @Test
+    public void example_test() {
+        assertEquals(true, false);
+    }
+
+}
